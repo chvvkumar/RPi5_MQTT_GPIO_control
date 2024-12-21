0d28db6cc8d2c2d398d2e024c2583642d232dcbc
chvvkumar <chvvkumar@gmail.com>
2024-12-21 11:55:15 -0600

    Replace print statements with logging for MQTT topics in gpiocontrol.py

--- gpiocontrol.py
+++ gpiocontrol.py
@@ -18,8 +18,8 @@ MQTT_USERNAME = config['MQTT'].get('username')
 MQTT_PASSWORD = config['MQTT'].get('password')
 publish_topic = config['MQTT']['publish_topic']
 
-print(f"MQTT topic: {MQTT_TOPIC}")
-print(f"Publish topic: {publish_topic}")
+logging.info(f"MQTT Topic: {MQTT_TOPIC}")
+logging.info(f"MQTT Publish Topic: {publish_topic}")
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
